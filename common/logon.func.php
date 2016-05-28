dc4372ef0823197015ffd227781c15b588a204b2
Morten Jakeobsen <morten@jakeobsen.com>
2016-05-28 14:37:15 +0200

    Split validate () into smaller functions.
    Validate have been split to three major functions.
    
    validateToken
     - Validates token set in session
    
    validateLogin
     - Validates username and password post data
    
    generateToken
     - Generates a new token for a given user

--- common/logon.func.php
+++ common/logon.func.php
@@ -1,10 +1,8 @@
 <?PHP
 namespace shibboleet\logon;
 
-function validate ()
+function install ()
 {
-  global $db;
-
   /* Install table if it dosen't exists. */
   \shibboleet\db\create_table_if_not_exists('users',"CREATE TABLE `users` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -14,20 +12,34 @@ function validate ()
     `token` varchar(240) NOT NULL,
     PRIMARY KEY (`id`)
   ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;") or die("Couldn't create table 'users'.");
+}
 
-  /* Check for logout request
-  *  Using a post till i figgure out how to omit a stupid "bug" in Safari. */
-  if ( isset ( $_POST['signout'] ) )
+function validate ()
+{
+  global $db;
+
+  install ();
+
+  if ( signout () )
   {
-    destroySession ();
-    header( 'Location: /' );
     return false;
   }
+  elseif ( validateToken () )
+  {
+    return true;
+  }
+  elseif ( validateLogin () )
+  {
+    return true;
+  }
+  return false;
+}
 
-  /* Check if session token is set */
-  $genToken = false;
+function validateToken()
+{
   if ( isset ( $_SESSION['token'] ) )
   {
+    global $db;
     $token = $db->real_escape_string ( $_SESSION['token'] );
     $result = $db->query ( "select token from `users` where `token`='$token' and `enabled`='1' limit 1" );
     if ( $result->num_rows > 0 )
@@ -35,36 +47,57 @@ function validate ()
     else destroySession ();
     unset ( $result );
   }
-  elseif ( isset ( $_POST['username'] ) && isset ( $_POST['password'] ) )
-    {
-      $username = $db->real_escape_string ( $_POST['username'] );
-      $result = $db->query ( "select id,password from `users` where `username`='$username' and `enabled`='1' limit 1;" );
-      if ( $result->num_rows > 0 )
-      {
-        $row = $result->fetch_assoc ();
-        unset ( $result );
-        $id = $row['id'];
-        $hash = $row['password'];
-        unset ( $row );
-        if ( password_verify ( $_POST['password'], $hash ) )
-        {
-          $genToken = true;
-        }
-        else
-        {
-          \shibboleet\debug\log ( "Failed login attempt." );
-          destroySession ();
-        }
-      }
-      else destroySession ();
-    }
+  return false;
+}
 
-  if ( $genToken == true )
+function validateLogin()
+{
+  if ( isset ( $_POST['username'] ) && isset ( $_POST['password'] ) )
   {
-    $token = $db->real_escape_string ( base64_encode ( openssl_random_pseudo_bytes ( 30 ) ) );
-    $_SESSION['token'] = $token;
-    $db->query ( "update `users` set `token`='$token' where `id`='$id';" );
+    global $db;
+    $username = $db->real_escape_string ( $_POST['username'] );
+    $result = $db->query ( "select id,password from `users` where `username`='$username' and `enabled`='1' limit 1;" );
+    if ( $result->num_rows > 0 )
+    {
+      $row = $result->fetch_assoc ();
+      unset ( $result );
+      $id = $row['id'];
+      $hash = $row['password'];
+      unset ( $row );
+      if ( password_verify ( $_POST['password'], $hash ) )
+      {
+        generateToken ( $id );
+        return true;
+      }
+      else
+      {
+        \shibboleet\debug\log ( "Failed login attempt." );
+        destroySession ();
+      }
+    }
+    else destroySession ();
+  }
+  return false;
+}
+
+function generateToken ( $id )
+{
+  global $db;
+  $token = $db->real_escape_string ( base64_encode ( openssl_random_pseudo_bytes ( 30 ) ) );
+  $_SESSION['token'] = $token;
+  $db->query ( "update `users` set `token`='$token' where `id`='$id';" );
+  header( 'Location: /' );
+}
+
+function signout()
+{
+  /* Check for logout request
+  *  Using a post till i figgure out how to omit a stupid "bug" in Safari. */
+  if ( isset ( $_POST['signout'] ) )
+  {
+    destroySession ();
     header( 'Location: /' );
+    return true;
   }
   return false;
 }
