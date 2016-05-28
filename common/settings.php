d162c0da3ffe8dd9f42da2a6604c06e5cdd62107
Morten Jakeobsen <morten@jakeobsen.com>
2016-05-28 11:54:31 +0200

    Beautify settings file.
    Yes. I like spaces around brackets.

--- common/settings.php
+++ common/settings.php
@@ -1,22 +1,22 @@
 <?PHP
 
 /* Debug */
-define( 'DEBUG_ENABLED', true );
+define ( 'DEBUG_ENABLED', true );
 
 /* MySQL Database Connection */
-define('DB_USER','root');
-define('DB_PASS','root');
-define('DB_HOST','127.0.0.1');
-define('DB_NAME','shibboleet');
+define ( 'DB_USER','root' );
+define ( 'DB_PASS','root' );
+define ( 'DB_HOST','127.0.0.1' );
+define ( 'DB_NAME','shibboleet' );
 
 /* Template */
-define('TEMPLATE','default');
+define ( 'TEMPLATE','default' );
 
 /* Default Page */
-define('PAGE_DEFAULT','home');
-define('PAGE_404','404');
+define ( 'PAGE_DEFAULT','home' );
+define ( 'PAGE_404','404' );
 
 /* Other things here... */
-define('DIR_PLUGINS','common/plugins/');
-define('DIR_TEMPLATES','common/templates/' . TEMPLATE);
-define('DIR_PAGES','common/pages/');
+define ( 'DIR_PLUGINS','common/plugins/' );
+define ( 'DIR_TEMPLATES','common/templates/' . TEMPLATE );
+define ( 'DIR_PAGES','common/pages/' );
