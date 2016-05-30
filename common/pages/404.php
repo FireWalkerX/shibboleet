9c5b60384573fd475e59db3499414b3bbe772bf5
Morten Jakeobsen <morten@jakeobsen.com>
2016-05-30 18:07:21 +0200

    Customize 404 page.

--- common/pages/404.php
+++ common/pages/404.php
@@ -1,1 +1,21 @@
-Erro 404 - not found!
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Home :: Shibboleet</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    * {font-family: arial;}
+    #err {margin: 0 auto; max-width: 360px; border: 1px solid #000; padding: 10px;}
+  </style>
+</head>
+<body>
+<div id="err">
+<h1>Error 404 - not found!</h1>
+
+<p>Dang! It seems that we lost'er - this is a sad day.</p>
+
+<p>May we offer you a link to the <a href="/">front page</a> perhaps?</p>
+</div>
+</body>
+</html>
