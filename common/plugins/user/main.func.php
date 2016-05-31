420b8081f9e991dc92d4a64662db16a080030fde
Morten Jakeobsen <morten@jakeobsen.com>
2016-05-31 17:12:41 +0200

    Add get_user function

--- common/plugins/user/main.func.php
+++ common/plugins/user/main.func.php
@@ -3,19 +3,32 @@ namespace shibboleet\plugin\user;
 
 function __plugin_init ()
 {
-    /* Stop executing if table is missing */
-    if ( !\shibboleet\db\table_exists ( 'users' ) ) die ( 0 );
+  /* Stop executing if table is missing */
+  if ( !\shibboleet\db\table_exists ( 'users' ) ) die ( 0 );
 }
 
 function get_users ()
 {
-    global $db;
-    /* Get all tables */
-    $result = $db->query( "select * from users;" );
-    if ( $result->num_rows > 0 )
-        while ( $row = $result->fetch_assoc () )
-            foreach($row as $id => $data)
-                $user[$row['id']][$id] = $data;
+  global $db;
+  /* Get all tables */
+  $user = false;
+  $result = $db->query( "select * from users;" );
+  if ( $result->num_rows > 0 )
+    while ( $row = $result->fetch_assoc () )
+      foreach($row as $id => $data)
+        $user[$row['id']][$id] = $data;
+  return $user;
+}
 
-    return $user;
+function get_user ( $id = false )
+{
+  if ( $id == false ) return false;
+  global $db;
+  /* Get all tables */
+  $row = false;
+  $id = $db->real_escape_string ( $id );
+  $result = $db->query( "select * from users where ID = '$id';" );
+  if ( $result->num_rows > 0 )
+    $row = $result->fetch_assoc ();
+  return $row;
 }
