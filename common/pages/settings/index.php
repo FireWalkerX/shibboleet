2cdef8c312b92696ead784ec290fea0d6701ff8f
Morten Jakeobsen <morten@jakeobsen.com>
2016-05-31 17:13:51 +0200

    Improve title for settings section

--- common/pages/settings/index.php
+++ common/pages/settings/index.php
@@ -1,10 +1,12 @@
 <?PHP \shibboleet\plugin\set_dependencies( 'template', 'user' ) ?>
-<?=\shibboleet\plugin\template\get_header ( 'Settings' , true , true )?>
+<?=\shibboleet\plugin\template\get_header (
+  ( isset ( $_GET['s'] ) ? ucfirst ( $_GET['s'] ) . ' :: ' : '' ) . 'Settings' , true , true )?>
 
 <h3>Settings</h3>
 
 <div class="sidebarMenu">
 <ul>
+  <li><a href="/?p=settings">General</a></li>
   <li><a href="/?p=settings&s=users">Users</a></li>
 </ul>
 </div>
