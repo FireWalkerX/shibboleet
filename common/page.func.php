ce586abc14c5beb017f2f916a00171302ed8b477
Morten Jakeobsen <morten@jakeobsen.com>
2016-05-31 13:58:26 +0200

    implement better subpage system

--- common/page.func.php
+++ common/page.func.php
@@ -5,7 +5,7 @@ function get_page_request ( $type = 'p' )
 {
   if ( isset ( $_GET[$type] ) ) $request = $_GET[$type];
   elseif ( isset ( $_POST[$type] ) ) $request = $_POST[$type];
-  else $request = PAGE_DEFAULT;
+  else $request = false;
   return $request;
 }
 
@@ -13,13 +13,21 @@ function get_page_file ( $subpage )
 {
   // Define files to check
   $pages = array (
-    'requested' => DIR_PAGES . get_page_request () . ".php",
+    'requested' => DIR_PAGES . get_page_request () . "/index.php",
+    'fallback'  => DIR_PAGES . get_page_request () . ".php",
     '404'       => DIR_PAGES . PAGE_404 . ".php",
-    'default'   => DIR_PAGES . PAGE_DEFAULT . ".php"
   );
 
   if ( $subpage == true )
-    $pages['requested'] = DIR_PAGES . get_page_request ('s') . ".php";
+  {
+    $pages['requested'] = DIR_PAGES . get_page_request ('p') . '/' . get_page_request ('s') . ".php";
+    if ( !isset ( $_GET['s'] ) )
+      $pages['fallback'] = DIR_PAGES . get_page_request ('p') . "/home.php";
+    /* We really don't want the user to request index, this will create an infinite loop, instead be nice
+     * to the user, and give them the home file instead. */
+    if($_GET['s'] == 'index')
+      $pages['requested'] = DIR_PAGES . get_page_request ('p') . "/home.php";
+  }
 
   // Try files, return path if exists
   foreach ( $pages as $page => $file) {
