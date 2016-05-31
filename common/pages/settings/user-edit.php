93a9e33ac59e4c1fbee687d6c2174746bf68fb66
Morten Jakeobsen <morten@jakeobsen.com>
2016-05-31 18:18:47 +0200

    update user-edit page

--- common/pages/settings/user-edit.php
+++ common/pages/settings/user-edit.php
@@ -1,16 +1,29 @@
 <?PHP \shibboleet\plugin\set_dependencies( 'template', 'user' ) ?>
-<?PHP $user = \shibboleet\plugin\user\get_user ( $_GET['id'] )?>
+<?PHP
+if (
+  isset ( $_POST['name'] )
+  && isset ( $_POST['email'] )
+) \shibboleet\plugin\user\update_user (
+  $_GET['id'],
+  array
+  (
+    'name' => $_POST['name'],
+    'email' => $_POST['email'],
+  )
+);
+?>
 
+
+<?PHP $user = \shibboleet\plugin\user\get_user ( $_GET['id'] )?>
+<form method="post">
 <p>ID: <?=$user['id']?></p>
-<p>Username: <input type="text" value="<?=$user['username']?>" /></p>
-<p>Name: <input type="text" value="<?=$user['name']?>" /></p>
-<p>E-mail: <input type="text" value="<?=$user['email']?>" /></p>
+<p>Username: <?=$user['username']?></p>
+<p>Name: <input type="text" name="name" value="<?=$user['name']?>" /></p>
+<p>E-mail: <input type="text" name="email" value="<?=$user['email']?>" /></p>
 <p>Enabled: <?=( $user['enabled'] == 1 ? "Yes" : "No" )?></p>
 <p>Been logged in: <?=( empty ( $user['token'] ) ? "No" : "Yes" )?></p>
-
-
-
-
+<button type="submit">Update userdata</button>
+</form>
 <br />
 <br />
 <br />
